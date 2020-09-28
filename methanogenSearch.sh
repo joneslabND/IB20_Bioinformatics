99b557b3cfb350379df0f579b13c13c85295ff9a
Ziyu Zeng <zzeng4@crcfe02.crc.nd.edu>
2020-09-28 11:10:26 -0400

    Added code for merging reference sequences

--- methanogenSearch.sh
+++ methanogenSearch.sh
@@ -1,4 +1,10 @@
 # This script...
 # Usage:
+# Merge reference sequences into a single file
+for file in ref_sequences/hsp70gene_*.fasta
+do sed '/^>/d' $file >> hsp70ref.fasta
+done
 
-
+for file in ref_sequences/mcrAgene_*.fasta
+do sed '/^>/d' $file >> mcrAref.fasta
+done
