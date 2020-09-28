e4a8fe4c6dc2d6155b8ca2c159a0f415836a9412
Jake Fuehrmeyer <jfuehrm2@crcfe01.crc.nd.edu>
2020-09-28 10:46:33 -0400

    Initial Script Usage Description

---
+++ Me-phresistance.sh
@@ -0,0 +1,7 @@
+#usage: Identifying methanogens by isolating the methyl-coenzyme M reductase
+(McrA) gene, which catalyzes the last step of methanogenesis and is
+ highly conserved across all methanogens.
+ Also identifying pH resistance by isolating the number of copies of the HSP70
+ gene, which is involved in protein biogenesis and refolding for stress
+ resistance.
+
