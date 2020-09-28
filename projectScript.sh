2870ee7ead182844edd11b4e681771b993f26339
Hope Baldwin <hbaldwi2@crcfe01.crc.nd.edu>
2020-09-28 11:30:57 -0400

    muscle step

--- projectScript.sh
+++ projectScript.sh
@@ -1,1 +1,17 @@
 
+var=1;
+
+for file in ref_sequences/hsp*.fasta
+do
+	../../bin/muscle -in $file -out hsp$var
+	var=$((var+1))
+done
+
+var=1;
+for file in ref_sequences/mcr*.fasta
+do 
+        ../../bin/muscle -in	$file -out mcr$var
+        var=$((var+1))
+done
+
+	
