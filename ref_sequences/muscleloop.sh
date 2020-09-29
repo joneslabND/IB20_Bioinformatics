0703be2c0a05283162ca660938407cfc4bf7bc9b
o0michelley <71276776+o0michelley@users.noreply.github.com>
2020-09-29 09:53:30 -0400

    Delete muscleloop.sh

--- ref_sequences/muscleloop.sh
+++
@@ -1,8 +0,0 @@
-# runs muscle on all refseq fasta files and ouputs the alignment
-# usage: bash muscleloop.sh in ref_sequences
-
-for file in ref_sequences
-do
-./muscle -in $file -out $filealigned.fasta
-done
-
