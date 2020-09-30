8370bac975cc7d5affa85d5c53aa5abc126ef322
Connor Evans <cevans7@crcfe01.crc.nd.edu>
2020-09-30 10:35:33 -0400

    Made single carats for the single sequence files

--- methanogenSearch.sh
+++ methanogenSearch.sh
@@ -1,4 +1,6 @@
-# This script...
+# This script takes reference sequences for the McrA gene and the HSP70 gene, aligns them,
+# builds a hidden markov model (HMM), and searches 50 candidate proteome sequences for these genes using the HMM
+
 # Usage: bash methanogenSearch.sh 
 
 # Make sure you have muscle, hmmsearch, hmmbuild, and the proteome and reference
@@ -6,9 +8,11 @@
 
 # Merge reference sequences into a single file
 for file in ref_sequences/hsp70gene_*.fasta
-do sed '/^>/d' $file >> hsp70ref.fasta
+do sed '/^>/d' $file > hsp70ref.fasta
 done
 
 for file in ref_sequences/mcrAgene_*.fasta
-do sed '/^>/d' $file >> mcrAref.fasta
+do sed '/^>/d' $file > mcrAref.fasta
 done
+
+
