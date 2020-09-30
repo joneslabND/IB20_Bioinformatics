74e60526624145da23d7801c74640f6fb6d7989e
Jenna Lehn <jlehn@crcfe02.crc.nd.edu>
2020-09-30 01:26:31 -0400

    aligned 18 and 22 not here bc too big to be pushed to github, not sure if this is right but here we are

--- bestmethane.sh
+++ bestmethane.sh
@@ -1,1 +1,21 @@
-It begins
+# Analyzes 50 proteomes for conserved mcrA gene for methoanogenesis and HSP70 gene for pH resistance and puts summary in table in text file
+
+# usage: bash bestmethane.sh <proteome.fasta>
+
+# for loop to find aligned sequences in proteomes
+
+for file in proteomes/proteome_*
+do
+~/bin/muscle -in $file -out aligned$file -maxiters 1 -diags1 -sv
+done
+
+# for loop to match conserved sequences
+
+for file in aligned*
+do
+~/bin/hmmer-3.3.1/src/hmmbuild HMM$file $file
+done
+
+# for loop to search sequence for conserved mcrA and HSP70 genes
+
+
