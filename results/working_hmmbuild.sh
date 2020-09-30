0e1f1415e889fdb5fbe82a3c00a9f4865120f90e
Laura Carroll <lcarrol3@crcfe01.crc.nd.edu>
2020-09-30 11:08:55 -0400

    Results for how hmmbuild works

---
+++ results/working_hmmbuild.sh
@@ -0,0 +1,5 @@
+# This script is a copy of what I've done to understand how "hmmbuild" works
+# Set up so that the script can be run from any place
+# Usage: bash working_hmmbuild.sh
+
+~/bin/hmmer/hmmbuild ~/Private/IB20_Bioinformatics/ref_sequences/aligned_hsp_refs_profile.hmm ~/Private/IB20_Bioinformatics/ref_sequences/aligned_hsp_refs.txt
