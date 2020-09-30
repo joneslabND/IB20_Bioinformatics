11892e27cbe93fb1cf0fe451616b2273b9ff04a4
Connor Evans <cevans7@crcfe01.crc.nd.edu>
2020-09-30 10:26:57 -0400

    More comments

--- methanogenSearch.sh
+++ methanogenSearch.sh
@@ -1,5 +1,9 @@
 # This script...
-# Usage:
+# Usage: bash methanogenSearch.sh 
+
+# Make sure you have muscle, hmmsearch, hmmbuild, and the proteome and reference
+# sequence folders in your current directory
+
 # Merge reference sequences into a single file
 for file in ref_sequences/hsp70gene_*.fasta
 do sed '/^>/d' $file >> hsp70ref.fasta
