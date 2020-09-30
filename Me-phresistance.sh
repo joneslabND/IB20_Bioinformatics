2eff2f7e13de8229e58e45e0aa3ceaa6a3287123
Kevin Catalfano <kcatalfa@crcfe01.crc.nd.edu>
2020-09-30 10:52:27 -0400

    updated usage instructions

--- Me-phresistance.sh
+++ Me-phresistance.sh
@@ -1,7 +1,8 @@
-#usage: Identifying methanogens by isolating the methyl-coenzyme M reductase
-(McrA) gene, which catalyzes the last step of methanogenesis and is
- highly conserved across all methanogens.
- Also identifying pH resistance by isolating the number of copies of the HSP70
- gene, which is involved in protein biogenesis and refolding for stress
- resistance.
+#usage: bash Me-phresistance ref seq combo files for Hsp90 and McrA genes. Lets us id matches in experimental proteomes
+# Identifying methanogens by isolating the methyl-coenzyme M reductase
+#(McrA) gene, which catalyzes the last step of methanogenesis and is
+# highly conserved across all methanogens.
+# Also identifying pH resistance by isolating the number of copies of the HSP70
+# gene, which is involved in protein biogenesis and refolding for stress
+# resistance.
 
