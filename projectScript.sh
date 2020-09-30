246ded5ae58b0cb55cc316d1f96f1b0e9ba5c28e
Hope Baldwin <hbaldwi2@crcfe01.crc.nd.edu>
2020-09-30 11:00:45 -0400

    tables

--- projectScript.sh
+++ projectScript.sh
@@ -1,11 +1,18 @@
 
 var=1;
+var2=1;
 
 for file in ref_sequences/hsp*.fasta
 do
 	../../bin/muscle -in $file -out hsp$var
 	../../bin/hmmbuild hspBuild$var hsp$var
+ 	for proteome in proteomes/*.fasta
+        do
+          	../../bin/hmmsearch --tblout hsp${var}Prot${var2}Table hspBuild$var $proteome
+                var2=$((var2+1))
+        done
 	var=$((var+1))
+        var2=1;
 done
 
 var=1;
