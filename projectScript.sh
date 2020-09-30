d4136d084a2fe5d152f173be9b1813478e423b40
Hope Baldwin <hbaldwi2@crcfe01.crc.nd.edu>
2020-09-30 10:22:31 -0400

    hmmbuild results

--- projectScript.sh
+++ projectScript.sh
@@ -4,6 +4,7 @@ var=1;
 for file in ref_sequences/hsp*.fasta
 do
 	../../bin/muscle -in $file -out hsp$var
+	../../bin/hmmbuild hspBuild$var hsp$var
 	var=$((var+1))
 done
 
@@ -11,7 +12,7 @@ var=1;
 for file in ref_sequences/mcr*.fasta
 do 
         ../../bin/muscle -in	$file -out mcr$var
-        var=$((var+1))
-done
+	../../bin/hmmbuild mcrBuild$var mcr$var        
+	var=$((var+1))
 
-	
+done
