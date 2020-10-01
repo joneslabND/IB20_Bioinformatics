d92d45121bebcdc9fcdcab950b8fc4e866273d36
Parinda Tennakoon <ptennako@crcfe02.crc.nd.edu>
2020-10-01 17:34:30 -0400

    complete script

---
+++ finder.sh
@@ -0,0 +1,28 @@
+#make a hsp70refs file  and mcrArefs file each containing all the reference sequence for each gene.
+cat ref_sequences/hsp70gene_[0-9][0-9].fasta > hsp70refs
+cat ref_sequences/mcrAgene_[0-9][0-9].fasta > mcrArefs
+
+#align reference sequence for the two gene from each file containing reference sequencea for each gene and make two files named 'hsp70muscleresult' and 'mcrAmuscleresult'
+muscle -in hsp70refs -out hsp70muscleresult
+muscle -in mcrArefs -out mcrAmuscleresult
+
+#Generate two HMM profiles for hsp70 and mcrA genes separately in two files named 'hsp70hmmbuild' and 'mcrAhmmbuild'
+../../../bin/hmmer/bin/hmmbuild hsp70hmmbuild hsp70muscleresult
+../../../bin/hmmer/bin/hmmbuild mcrAhmmbuild mcrAmuscleresult
+
+#Search each proteome in the proteomes directory for matches for each gene using the HMM profiles and output a seaparate file with matches for each gene.
+cd proteomes
+for file in *.fasta
+do
+../hmmsearch --tblout hsp70_$file ../hsp70hmmbuild $file
+../hmmsearch --tblout mcrA_$file ../mcrAhmmbuild $file
+done
+
+#Search each result file and print out the proteome name and the matches for hsp70 gene followed by the matches for the mcrA gene.
+for file in proteome_{01..50}.fasta
+do
+match1=$(cat hsp70_$file | grep -v '#' | wc -l)
+match2=$(cat mcrA_$file | grep -v '#' | wc -l)
+echo "$file,$match1,$match2" | sed 's/.fasta//g'
+done
+
