abdca02c1392eb485e4c680605e48a5a2a1e6aab
Parinda Tennakoon <ptennako@crcfe02.crc.nd.edu>
2020-10-01 17:40:52 -0400

    changes

--- hmmseachscript.sh
+++
@@ -1,5 +0,0 @@
-for file in proteomes/*.fasta
-do
-./hmmsearch --tblout ./hsp70_$file hsp70hmmbuild $file
-./hmmsearch --tblout ./mcrA_$file mcrAhmmbuild $file
-done
--- hmmsearchscript.sh
+++
@@ -1,7 +0,0 @@
-for file in proteomes/*.fasta
-do
-./hmmer/bin/hmmsearch --tblout hsp70_$file hsp70hmmbuild $file
-./hmmer/bin/hmmsearch --tblout mcrA_$file mcrAhmmbuild $file
-done
-
- 
