e0c8d36f5ba6627bc59ce6478770638a3333d6d9
Laura Carroll <lcarrol3@crcfe01.crc.nd.edu>
2020-10-01 19:09:08 -0400

    Added in section to compile gene files

--- ref_sequences/compilerefs.sh
+++ ref_sequences/compilerefs.sh
@@ -1,7 +1,15 @@
-#Usage: bash compilehsp.sh ReferencesToCompile NameForCompiledFile
-#compile the reference sequences for genes
+#Usage: bash compilehsp.sh hspGeneFiles mcrAGeneFiles
+# Compile the reference sequences for the hsp proteins
+# Edit this section to change which genes to look for
 for reference in $1
 do
 echo $reference
-cat $reference >> $2.txt
+cat $reference >> aligned_hsp_refs.txt
+done
+
+# Compiles the reference sequences for the mcrA gene
+# Edit or comment this section if you want to change which genes to look for
+for reference2 in $2
+do
+cat $reference2 >> aligned_mcrA_refs.txt
 done
--- results/final_script.sh
+++ results/final_script.sh
@@ -1,5 +1,20 @@
 # This is the final script for the Bioinformatics project
-# Usage: bash final_script.sh 
+# Usage: bash final_script.sh hspGeneFiles mcrAGeneFiles
+
+# Compile the reference sequences for the hsp proteins
+# Edit this section to change which genes to look for
+for reference in $1
+do
+echo $reference
+cat $reference >> aligned_hsp_refs.txt
+done
+
+# Compiles the reference sequences for the mcrA gene
+# Edit or comment this section if you want to change which genes to look for
+for reference2 in $2
+do
+cat $reference2 >> aligned_mcrA_refs.txt
+done
 
 # This part uses the tool hmmbuild to align the sequences for the different proteins. It's split into two lines so
 # that it creates a search image for the hsp proteins and mrcA proteins
