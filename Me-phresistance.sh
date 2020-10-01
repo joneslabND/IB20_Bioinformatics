fb71d062d3cd470ea5d9cccfcaacd8403d60b49d
Jake Fuehrmeyer <jfuehrm2@crcfe01.crc.nd.edu>
2020-09-30 21:49:27 -0400

    notes for what is going to change in 'for loop' when we use the tools

--- Me-phresistance.sh
+++ Me-phresistance.sh
@@ -6,3 +6,17 @@
 # gene, which is involved in protein biogenesis and refolding for stress
 # resistance.
 
+
+../muscle -in referencefile -out alignment.afa
+(this will stay the same except we will probably have to change the reference sequence we are 
+looking at)
+
+../hmmbuild modelresults alignment being used
+(this will stay the same except we will probably have to change the file name for the models we 
+build)  
+
+../hmmsearch --tblout 3mcrsearch.afa HMMmodelmcr.afa ../proteomes/proteome_03.fasta
+../hmmsearch --tblout 2mcrsearch.afa HMMmodelmcr.afa ../proteomes/proteome_02.fasta
+(the output file will always change, the model being used will come from the result of hmmbuild
+of the specific alignment and the path I used is from the tool directory to the proteomes
+directory)
