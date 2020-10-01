315583c6ab0159c844342e786d42b4c2551768f2
Kyle Walker <kwalke11@crcfe02.crc.nd.edu>
2020-10-01 17:36:00 -0400

    updated script

--- project.sh
+++ project.sh
@@ -21,19 +21,20 @@ for number in {01..50}; do cat proteome_$number.fasta | sed "s/>/>proteome_$numb
 
 cd ../working_files
 
+#find proteomes with mcrA gene matches
 ../../bin/./muscle -in mcrAsequences.fasta -out mcrAgenefull_alignment
 ../../bin/./hmmbuild mcrAgene_build mcrAgenefull_alignment
 ../../bin/./hmmsearch --tblout mcrA_match.tbl mcrAgene_build proteomedatabase.fasta 
 
-# need to get results from mcrA into file that hmmer can read
+# find proteomes with hsp70 gene matches
 ../../bin/muscle -in hsp70sequences.fasta -out hsp70genefull_alignment
 ../../bin/hmmbuild hsp70gene_build hsp70genefull_alignment
-../../bin/hmmsearch --tblout hsp70_match.tbl hsp70gene_build [[[[[results from mcrA]]]
+../../bin/hmmsearch --tblout hsp70_match.tbl hsp70gene_build proteomedatabase.fasta
 
-# create files with candidates for each gene
+# create text  files with candidates for each gene
 grep -v "#" mcrA_match.tbl | cut -d " " -f 1 | tr -d "WP" >> mcrAcandidates.txt
 grep -v "#" hsp70_match.tbl | cut -d " " -f 1 | tr -d [WNY]P >> hsp70candidates.txt
 
-# compare files to see which proteomes contain both genes
+# compare files to see which proteomes contain both genes and create a text final with pH resistant methanogens
 grep -f mcrAcandidates.txt hsp70candidates.txt | sort | uniq >> FinalCandidates.txt 
 
