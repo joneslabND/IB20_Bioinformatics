cf35c1f3ad2129ff9aa03611efb484e5ee5ed275
Laura Carroll <lcarrol3@crcfe01.crc.nd.edu>
2020-10-01 18:48:31 -0400

    Search images go to results folder, matching hmmbuild pathways

--- results/final_script.sh
+++ results/final_script.sh
@@ -2,6 +2,6 @@
 # Usage: bash final_script.sh 
 
 # This part uses the tool hmmbuild to align the sequences for the different proteins. It's split into two lines so
-# that it creates a profile for the hsp proteins and mrcA proteins
-~/bin/hmmer/hmmbuild ~/Private/IB20_Bioinformatics/ref_sequences/aligned_hsp_refs_profile.hmm ~/Private/IB20_Bioinformatics/ref_sequences/aligned_hsp_refs.txt
-~/bin/hmmer/hmmbuild ~/Private/IB20_Bioinformatics/ref_sequences/aligned_mcrA_refs_profile.hmm ~/Private/IB20_Bioinformatics/ref_sequences/aligned_mcrA_refs.txt
+# that it creates a search image for the hsp proteins and mrcA proteins
+~/bin/hmmer/bin/hmmbuild ~/Private/IB20_Bioinformatics/results/aligned_hsp_refs_profile.hmm ~/Private/IB20_Bioinformatics/ref_sequences/aligned_hsp_refs.txt
+~/bin/hmmer/bin/hmmbuild ~/Private/IB20_Bioinformatics/results/aligned_mcrA_refs_profile.hmm ~/Private/IB20_Bioinformatics/ref_sequences/aligned_mcrA_refs.txt
