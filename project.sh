e24d6cf6d9b8f4d5509749bf1ab293f6acd586a8
Davin Lee <dlee27@crcfe02.crc.nd.edu>
2020-10-01 14:41:51 -0400

    reorganized and renamed muscle_files into working_files

--- project.sh
+++ project.sh
@@ -1,11 +1,16 @@
-#make a directory for  muscle and hmmer files under IB20_Bioinformatics
+# start in IB20_Bioinformatics directory
+# usage: 
+# output:
+mkdir working_files
+
+cd ref_sequences
 
 for sequence in mcrA*
 do
-cat $sequence >> ../muscle_files/mcrAsequences.txt
+cat $sequence >> ../working_files/mcrAsequences.txt
 done
 
 for sequence in hsp70*
 do
-cat $sequence >> ../muscle_files/hsp70sequences.txt
+cat $sequence >> ../working_files/hsp70sequences.txt
 done 
