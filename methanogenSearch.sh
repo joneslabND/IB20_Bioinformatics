06c155767a156064d292bb4d2ab6095665bbc55e
Connor Evans <cevans7@crcfe01.crc.nd.edu>
2020-10-02 10:15:28 -0400

    Working with hmmsearch

--- methanogenSearch.sh
+++ methanogenSearch.sh
@@ -9,6 +9,11 @@
 # remove files that are generated within the script, to avoid soft errors when re-running the script
 rm hsp70ref.fasta
 rm mcrAref.fasta
+rm mcrA.aligned
+rm hsp70.aligned
+rm mcrA.hmm
+rm hsp70.hmm
+rm hsp70results
 # rm proteomesOfInterest.txt - for when we generate the text file at the end
 
 # Merge reference sequences into a single file
@@ -25,12 +30,19 @@ done
 
 ./muscle -in mcrAref.fasta -out mcrA.aligned  #for McrA first on mcrAref.fasta, to align the sequences
 
-./hmmbuild mcrA.hmm mcrA.aligned  #to build a HMM based on the output of muscle, above
+./hmmbuild mcrA.hmm mcrA.aligned  #to build a HMM based on the output of muscle, in line above
 
 ./muscle -in hsp70ref.fasta -out hsp70.aligned #for Hsp70 gene, to align these sequences
 
-./hmmbuild hsp70.hmm hsp70.aligned  #to build a HMM based on the output of muscle, above
+./hmmbuild hsp70.hmm hsp70.aligned  #to build a HMM based on the output of muscle, in line above
 
-# ./hmmsearch, for 50 times per for the methanogen gene, then output 
+
+# ./hmmsearch, for 50 times per for the methanogen gene, then output proteomes that are methanogens 
+
+for file in ./proteomes/*.fasta
+do
+./hmmsearch hsp70results mcrA.hmm $file
+# need to put in text file or something 
+done
 
 # ./take only the proteomes that are identified as methanogens and then do ./hmmsearch again to find ph resistant sequences only in these proteomes
