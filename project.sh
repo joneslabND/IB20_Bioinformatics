dcec6963aa189b6f37a35a9d5a8f4325304e97d3
Kyle Walker <kwalke11@crcfe02.crc.nd.edu>
2020-10-02 13:45:08 -0400

    fixed grep

--- project.sh
+++ project.sh
@@ -32,8 +32,8 @@ cd ../working_files
 ../../bin/hmmsearch --tblout "$2"match.tbl "$2"gene_build ../proteomes/proteomedatabase.fasta
 
 # create text files with candidates for each gene
-grep -v "#" "$1"_match.tbl | cut -d "_" -f 1,2 | tr -d "WP" > "$1"candidates.txt
-grep -v "#" "$2"_match.tbl | cut -d "_" -f 1,2 | tr -d [WNY]P > "$2"candidates.txt
+grep -v "#" "$1"match.tbl | cut -d "_" -f 1,2 | tr -d "WP" > "$1"candidates.txt
+grep -v "#" "$2"match.tbl | cut -d "_" -f 1,2 | tr -d [WNY]P > "$2"candidates.txt
 
 # compare files to see which proteomes contain both genes and create a text final with pH resistant methanogens
 grep -f "$1"candidates.txt "$2"candidates.txt | sort | uniq > FinalCandidates.txt 
