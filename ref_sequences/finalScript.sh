b9631135eebda8cddc60e794e42b7fb139cb783e
Savannah Miracle <smiracle@crcfe02.crc.nd.edu>
2020-10-02 10:50:20 -0400

    adding scripts to final

--- ref_sequences/finalScript.sh
+++ ref_sequences/finalScript.sh
@@ -16,3 +16,20 @@ done
 #run muscle to align all mcrA reference files   
 ../muscle -in allmcrA.txt -out alignmcrA.afa
 
+#build hidden markov model for hsp70 references
+hmmbuild hsp.HMM alignhsp70.afa
+
+#build hidden markov model for mcrA references
+hmmbuild mcrA.HMM alignmcrA.afa
+
+#search	all 50 proteomes using hsp.HMM as search criteria
+for num in {01..50}
+do
+hmmsearch hsp.HMM proteome_$num.fasta >> hsp_proteomes.txt
+done
+
+#search all 50 proteomes using mcrA.HMM as search criteria
+for num in {01..50}
+do
+hmmsearch mcrA.HMM proteome_$num.fasta >> mcrA_proteomes.txt
+done
