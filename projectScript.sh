cd007baa8717f4719b844de37d9736676de5dabc
Hope Baldwin <hbaldwi2@crcfe01.crc.nd.edu>
2020-10-02 12:21:59 -0400

    Final Script

--- projectScript.sh
+++ projectScript.sh
@@ -1,25 +1,64 @@
+# Hope Baldwin and Morgan Hiller
+# usage: bash projectScript.sh
 
-var=1;
-var2=1;
-
-for file in ref_sequences/hsp*.fasta
+# combine all hsp sequences to one file
+for file in ref_sequences/hsp*
 do
-	../../bin/muscle -in $file -out hsp$var
-	../../bin/hmmbuild hspBuild$var hsp$var
- 	for proteome in proteomes/*.fasta
-        do
-          	../../bin/hmmsearch --tblout hsp${var}Prot${var2}Table hspBuild$var $proteome
-                var2=$((var2+1))
-        done
-	var=$((var+1))
-        var2=1;
+	cat $file >> combinedHSP.fasta
 done
 
-var=1;
-for file in ref_sequences/mcr*.fasta
-do 
-        ../../bin/muscle -in	$file -out mcr$var
-	../../bin/hmmbuild mcrBuild$var mcr$var        
-	var=$((var+1))
+# use muscle to align sequences
+./muscle -in combinedHSP.fasta -out alignedHSP
 
+# build from the alignment file 
+./hmmbuild buildHSP alignedHSP
+
+# combine all mcr sequences to one file
+for file in ref_sequences/mcr*
+do
+  	cat $file >> combinedMCR.fasta
 done
+
+# use muscle to align sequences
+./muscle -in combinedMCR.fasta -out alignedMCR
+
+# build from the alignment file
+./hmmbuild buildMCR alignedMCR 
+
+# create variable to count the proteomes
+var=1
+
+# loop through each proteome and run a hsp and mcr search on each one
+for file in proteomes/*.fasta
+do
+	./hmmsearch --tblout tableHSP${var} buildHSP $file
+	./hmmsearch --tblout tableMCR${var} buildMCR $file
+	var=$((var+1))
+done
+
+var=$((var-1))
+
+# Create final ouput table
+echo "Proteome #, HSP, MCR" > resultsTable.txt
+for num in $(seq 1 $var)
+do
+	# use grep to count the number of matches
+	hspCount=$(cat tableHSP$num | grep -v -c "#")  
+	mcrCount=$(cat tableMCR$num | grep -v -c "#")
+	
+	# store in final table
+	echo "$num, $hspCount, $mcrCount" >> resultsTable.txt
+	
+done 
+
+# Put the proteomes that have at least 1 mcr and at least 2 hsp into a text file
+echo "Proteome numbers of the candidate pH-resistant methanogens" > resultProteomes.txt
+cat resultsTable.txt | grep -E "[0-9]+, [2-N], [1-N]" | cut -d , -f 1 >> resultProteomes.txt
+
+
+# delete combined files so when we run the code again it will not append on to the old ones
+rm combinedHSP.fasta
+rm combinedMCR.fasta
+
+
+
