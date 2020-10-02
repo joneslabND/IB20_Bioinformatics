6b5af1f2d3e585c911218310da6f424d3675aac0
Jordan Brown <jbrown36@crcfe01.crc.nd.edu>
2020-10-02 12:55:50 -0400

    Specified comments and added arguments

--- finalscript.sh
+++ finalscript.sh
@@ -1,16 +1,18 @@
-#Usage:
+#Identifying candidate translated genomes based on reference sequences
+#Usage: bash finalscript.sh pathtotoolsdirectory
+
 cd ref_sequences
 
-# Create template for hsp gene 
+# Create markov model for hsp gene 
 
 cat hsp*.fasta >> hspgenefull.fasta
-../../bin/muscle -in hspgenefull.fasta -out hspgenemuscle.txt
-../../bin/hmmbuild hspgenehmmr.txt hspgenemuscle.txt 
+"$1"/muscle -in hspgenefull.fasta -out hspgenemuscle.txt
+"$1"/bin/hmmbuild hspgenehmmr.txt hspgenemuscle.txt 
 
-# Create template for mcrA gene
+# Create markov model for mcrA gene
 cat mcrA*.fasta >> mcrAgenefull.fasta
-../../bin/muscle -in mcrAgenefull.fasta -out mcrAgenemuscle.txt
-../../bin/hmmbuild mcrAgenehmmr.txt mcrAgenemuscle.txt
+"$1"/bin/muscle -in mcrAgenefull.fasta -out mcrAgenemuscle.txt
+"$1"/bin/hmmbuild mcrAgenehmmr.txt mcrAgenemuscle.txt
 
 #Execute searches for both genes
 cd ../proteomes
@@ -20,17 +22,17 @@ mkdir proteomemcrA
 cd proteomeshsp
 for file in *.fasta
 do
-../../bin/hmmsearch --tblout proteomeshsp/hsp$file.txt ../ref_sequences/hspgenehmmr.txt $file
+"$1"/bin/hmmsearch --tblout proteomeshsp/hsp$file.txt ../ref_sequences/hspgenehmmr.txt $file
 done
 
 cd ../proteomesmcrA
 
 for file in *.fasta
 do
-../../bin/hmmsearch --tblout proteomemcrA/mcrA$file.txt ../ref_sequences/mcrAgenehmmr.txt $file
+"$1"/bin/hmmsearch --tblout proteomemcrA/mcrA$file.txt ../ref_sequences/mcrAgenehmmr.txt $file
 done 
 
-#Search for genes
+#Search each proteome for hsp and mcrA markov models
 
 for file in mcrAproteome*.fasta.txt
 do
