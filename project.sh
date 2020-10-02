dc2749004c22d61603abe253f87a13a65ed06e93
Davin Lee <dlee27@crcfe02.crc.nd.edu>
2020-10-02 11:23:00 -0400

    added arguments

--- project.sh
+++ project.sh
@@ -1,18 +1,18 @@
 # start in IB20_Bioinformatics directory
-# usage: 
+# usage: bash project.sh gene_1 gene_2
 # output:
 mkdir working_files
 
 cd ref_sequences
 
-for sequence in mcrA*
+for sequence in "$1"*
 do
-cat $sequence >> ../working_files/mcrAsequences.fasta
+cat $sequence >> ../working_files/"$1"sequences.fasta
 done
 
-for sequence in hsp70*
+for sequence in "$2"*
 do
-cat $sequence >> ../working_files/hsp70sequences.fasta
+cat $sequence >> ../working_files/"$2"sequences.fasta
 done 
 
 #create a proteomedatabase with all 50 proteomes
@@ -21,25 +21,25 @@ for number in {01..50}; do cat proteome_$number.fasta | sed "s/>/>proteome_$numb
 
 cd ../working_files
 
-#find proteomes with mcrA gene matches
-../../bin/./muscle -in mcrAsequences.fasta -out mcrAgenefull_alignment
-../../bin/./hmmbuild mcrAgene_build mcrAgenefull_alignment
-../../bin/./hmmsearch --tblout mcrA_match.tbl mcrAgene_build ../proteomes/proteomedatabase.fasta 
+#find proteomes with gene_1 matches
+../../bin/./muscle -in "$1"sequences.fasta -out "$1"genefull_alignment
+../../bin/./hmmbuild "$1"gene_build "$1"genefull_alignment
+../../bin/./hmmsearch --tblout "$1"_match.tbl "$1"gene_build ../proteomes/proteomedatabase.fasta 
 
-# find proteomes with hsp70 gene matches
-../../bin/muscle -in hsp70sequences.fasta -out hsp70genefull_alignment
-../../bin/hmmbuild hsp70gene_build hsp70genefull_alignment
-../../bin/hmmsearch --tblout hsp70_match.tbl hsp70gene_build ../proteomes/proteomedatabase.fasta
+# find proteomes with gene_2 matches
+../../bin/muscle -in "$2"sequences.fasta -out "$2"genefull_alignment
+../../bin/hmmbuild "$2"gene_build "$2"genefull_alignment
+../../bin/hmmsearch --tblout "$2"_match.tbl "$2"gene_build ../proteomes/proteomedatabase.fasta
 
-# create text  files with candidates for each gene
-grep -v "#" mcrA_match.tbl | cut -d "_" -f 1,2 | tr -d "WP" >> mcrAcandidates.txt
-grep -v "#" hsp70_match.tbl | cut -d "_" -f 1,2 | tr -d [WNY]P >> hsp70candidates.txt
+# create text files with candidates for each gene
+grep -v "#" "$1"_match.tbl | cut -d "_" -f 1,2 | tr -d "WP" > "$1"candidates.txt
+grep -v "#" "$2"_match.tbl | cut -d "_" -f 1,2 | tr -d [WNY]P > "$2"candidates.txt
 
 # compare files to see which proteomes contain both genes and create a text final with pH resistant methanogens
-grep -f mcrAcandidates.txt hsp70candidates.txt | sort | uniq >> FinalCandidates.txt 
+grep -f "$1"candidates.txt "$2"candidates.txt | sort | uniq > FinalCandidates.txt 
 
 #make a table with summary of all outputs
-for number in {01..50} ; do export hsp70var_$number=$(grep -c "proteome_$number" hsp70candidates.txt) ; done
-for number in {01..50} ; do export mcrAvar_$number=$(grep -c "proteome_$number" mcrAcandidates.txt) ; done
-for number in {01..50} ; do eval echo proteome_$number,\$hsp70var_$number,\$mcrAvar_$number; done >> SummaryTable.txt
-sed '1 s/^/proteome,hsp 70 matches,mcrA matches\n/' SummaryTable.txt > FinalSummaryTable.txt 
+for number in {01..50} ; do export "$2"var_$number=$(grep -c "proteome_$number" "$2"candidates.txt) ; done
+for number in {01..50} ; do export "$1"var_$number=$(grep -c "proteome_$number" "$1"candidates.txt) ; done
+for number in {01..50} ; do eval echo proteome_$number,\$"$2"var_$number,\$"$1"var_$number; done >> SummaryTable.txt
+sed '1 s/^/proteome,"$2" matches,"$1" matches\n/' SummaryTable.txt > FinalSummaryTable.txt 
