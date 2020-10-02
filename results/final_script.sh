49d792891b5a5df91bb2ec9ee060488af7685fad
Laura Carroll <lcarrol3@crcfe01.crc.nd.edu>
2020-10-01 22:07:16 -0400

    Added in muscle, fixed naming problems

--- results/final_script.sh
+++ results/final_script.sh
@@ -1,31 +1,32 @@
 # This is the final script for the Bioinformatics project
-# Usage: bash final_script.sh hspGeneFiles mcrAGeneFiles proteomeFiles
+# Usage: bash final_script.sh
 
 # Compile the reference sequences for the hsp proteins
 # Edit this section to change which genes to look for
-for reference in $1
+for reference in ../ref_sequences/"hsp70gene_*.fasta"
 do
 echo $reference
-cat $reference >> aligned_hsp_refs.txt
+cat $reference >> compiled_hsp.txt
 done
 
 # Compiles the reference sequences for the mcrA gene
 # Edit or comment this section if you want to change which genes to look for
-for reference2 in $2
+for reference2 in ../ref_sequences/"mcrAgene_*.fasta"
 do
-cat $reference2 >> aligned_mcrA_refs.txt
+cat $reference2 >> compiled_mrcA.txt
 done
+
+# Runs both of the compiled reference sequences through muscle to set them up
+# for the next step
+~/bin/muscle -in compiled_mrcA.txt -out aligned_mcrA_refs.txt
+~/bin/muslce -in compiled_hsp.txt -out aligned_hsp_refs.txt
 
 # This part uses the tool hmmbuild to align the sequences for the different proteins. It's split into two lines so
 # that it creates a search image for the hsp proteins and mrcA proteins
-~/bin/hmmer/bin/hmmbuild ~/Private/IB20_Bioinformatics/results/aligned_hsp_refs_profile.hmm ~/Private/IB20_Bioinformatics/ref_sequences/aligned_hsp_refs.txt
-~/bin/hmmer/bin/hmmbuild ~/Private/IB20_Bioinformatics/results/aligned_mcrA_refs_profile.hmm ~/Private/IB20_Bioinformatics/ref_sequences/aligned_mcrA_refs.txt
+~/bin/hmmer/bin/hmmbuild ./aligned_hsp_refs_profile.hmm ./aligned_hsp_refs.txt
+~/bin/hmmer/bin/hmmbuild ./aligned_mcrA_refs_profile.hmm ./aligned_mcrA_refs.txt
 
-# This is to compile the proteomes into one file
-for reference3 in $3
-do
-echo $reference3
-cat $reference3 >> compiled_proteomes.txt
-done
+# This part runs an hmmsearch
+# DOES NOT WORK AT THE MOMENT
 
 ~/bin/hmmer/bin/hmmsearch --tblout resultsfile.txt aligned_hsp_refs_profile.hmm compiled_proteomes.txt
