681af3be47ec7f8beacb7e8269c17d72cc4e5895
Madeline Galvin <mgalvin4@crcfe01.crc.nd.edu>
2020-10-02 11:10:19 -0400

    fix proteomes loop

--- bioinformatics.sh
+++ bioinformatics.sh
@@ -22,11 +22,19 @@ done
 # create hsp70 gene search image
 ~/Private/bin/bin/hmmbuild hsp70searchimage.hmm alignedhsp70genes.fasta
 
+# make a header for summary file
+echo "proteome, mcrA_Hits, hsp70_Hits" > summaryOutput.csv
+
 # search proteomes for mcrA image and hsp70 image
 for file in proteomes/proteome_*.fasta
 do
-~/Private/bin/bin/hmmsearch --tblout mcrAresults.fasta mcrAsearchimage.hmm $file
-~/Private/bin/bin/hmmsearch --tblout hsp70results.fasta hsp70searchimage.hmm $file
+name = $(echo $file | cut -d "." -f 1 | cut -d "/" -f 2)
+~/Private/bin/bin/hmmsearch --tblout mcrAresults."$name".txt mcrAsearchimage.hmm $file
+~/Private/bin/bin/hmmsearch --tblout hsp70results."$name".txt hsp70searchimage.hmm $file
+mcrAHits = $(cat mcrAresults."$name".txt | grep -v "#" | wc -l)
+hsp70Hits = $(cat hsp70results."$name".txt | grep -v "#" | wc -l)
+echo "$name,$,$" >> summaryOutput.csv
 done
 
 # count hits
+# sort summary, keep non zero in mcra, sort by hsp70
