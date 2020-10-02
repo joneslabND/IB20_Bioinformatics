2ff23f5afcb4f595b8b047bab47922a167e3ff20
Kevin Catalfano <kcatalfa@crcfe01.crc.nd.edu>
2020-10-02 10:19:55 -0400

    commented uncommented lines

--- Me-phresistance.sh
+++ Me-phresistance.sh
@@ -2,8 +2,8 @@
 #experimental proteomes
 # Identifying methanogens by isolating the methyl-coenzyme M reductase
 #(McrA) gene, which catalyzes the last step of methanogenesis and is highly conserved across all
-methanogens. Also identifying pH resistance by isolating the number of copies of the HSP70 gene,
-which is involved in protein biogenesis and refolding for stress resistance.
+#methanogens. Also identifying pH resistance by isolating the number of copies of the HSP70 gene,
+#which is involved in protein biogenesis and refolding for stress resistance.
 
 
 #../muscle -in referencefile -out alignment.afa
