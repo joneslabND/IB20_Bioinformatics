38a586f7d717e23a686776d1d29478b23d96ee77
Jake Fuehrmeyer <jfuehrm2@crcfe01.crc.nd.edu>
2020-10-02 16:39:46 -0400

    full coded bash script w/out full comments or variables added

--- bioinformatics.sh
+++ bioinformatics.sh
@@ -15,4 +15,68 @@ mkdir mcraoutputs
 #Within these directories, we ran the muscle tool and hmmr tools to produce all of our outputs that
 #we need to go into our for loop as inputs.
 
+#mkdir gene1outputs
+#mkdir gene2outputs
+
+#hsp70gene_combined.fasta mcrAgene+combined.fasta
+
+#cd ref_sequences
+
+#mv hsp70gene_combined.fasta ../gene1outputs
+#mv mcrAgene+combined.fasta ../gene2outputs
+
+#cd ..
+
+#~/muscle -in gene1outputs/hsp70gene_combined.fasta -out gene1alignment.afa > gene1outputs
+#~/muscle -in gene2outputs/mcrAgene+combined.fasta -out gene2alignment.afa > gene2outputs
+#~/hmmbuild HMMmodelgene1.afa gene1outputs/gene1alignment.afa
+#~/hmmbuild HMMmodelgene2.afa gene2outputs/gene2alignment.afa
+
+mkdir hsp70searches
+cd proteomes
+for proteome in *.fasta
+do
+cd ..
+~/hmmsearch --tblout hsp70searches/$proteome.hsp70txt gene1outputs/HMMmodelgene1.afa proteomes/$proteome
+cd proteomes
+done
+ 
+#echo "proteome, number of matches" >>hsp70matches.txt
+cd ../hsp70searches
+for proteomefile in *fasta.hsp70txt
+do
+var1=`cat $proteomefile | grep ^WP_ | wc -l`
+cd ..
+echo "$proteomefile, $var1" >>hsp70matches.txt
+cd hsp70searches
+done
+ 
+cd ..
+mkdir mcrasearches
+cd proteomes
+for proteome in *.fasta
+do
+cd ..
+~/hmmsearch --tblout mcrasearches/$proteome.mcratxt gene2outputs/HMMmodelgene2.afa proteomes/$proteome
+cd proteomes
+done
+ 
+#echo proteome, number of matches >mcramatches.txt
+cd ../mcrasearches
+for proteomefile in *fasta.mcratxt
+do
+var2=`cat $proteomefile | grep ^WP_ | wc -l`
+cd ..
+echo "$proteomefile, $var2" >>mcramatches.txt
+cd mcrasearches
+done
+ 
+cd ..
+ 
+ 
+
+
+cut -d "," -f 2 mcramatches.txt | paste -d, hsp70matches.txt - | sed 's/.fasta.hsp70txt//' > finalmatches.txt  
+
+cat finalmatches.txt | grep -v " 0" | cut -d "," -f 1 | sed 's/.fasta.hsp70txt//' > finalproteomelist.txt
 
