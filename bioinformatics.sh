4cfb3ff3137e0aa793dcef5195ffee2704b77db7
Madeline Galvin <mgalvin4@crcfe01.crc.nd.edu>
2020-10-02 13:44:54 -0400

    final edits

--- bioinformatics.sh
+++ bioinformatics.sh
@@ -1,3 +1,8 @@
+# this script uses muscle to align proteome gene sequences
+# it then uses hmmer to create a search image for the mcrA gene of methanogens, and the hsp70 gene showing pH resistance
+# it outputs results files including tables for both mcrA gene and hsp70 gene matches
+# it then cuts and sorts the results to create the file pHresistantProteomes.txt to show which methanogen proteomes have the highest pH resistance
+
 # mcrA total genes file
 for file in ref_sequences/mcrAgene*.fasta
 do
@@ -36,6 +41,6 @@ hsp70Hits=$(cat hsp70results."$name".txt | grep -v "#" | wc -l)
 echo "$name,$mcrAHits,$hsp70Hits" >> summaryOutput.csv
 done
 
-# include only proteomes with mcrA genes, sort by highest copies of hsp70 gene, store in final text file
+# include only proteomes with mcrA genes (methanogens), sort by highest copies of hsp70 gene (pH resistance), store in final text file
 cat summaryOutput.csv | sort -t , -n -r -k 2 | head -n 16 | sort -t , -n -r -k 3 >> pHresistantProteomes.txt
 
