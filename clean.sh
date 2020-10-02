1ad9a3630cf446277a791bfea8a13add5f6ad200
nolan-downey <56741980+nolan-downey@users.noreply.github.com>
2020-10-02 14:20:25 -0400

    Add files via upload
    Git Work Around, Final Commits

---
+++ clean.sh
@@ -0,0 +1,8 @@
+#Short script to clean the files and directories created by solution.sh
+
+rm hmmer_profile_*
+rm -r hmmer_search_results_*
+rm $1_combined_sequence.txt
+rm $2_combined_sequence.txt
+rm muscle_result_*
+
